b80d395ae00524ac5beaaa8e006f3b201d461e0c
Grégory Soupé <gregory.soupe@mailo.com>
2023-10-17 15:37:27 +0200

    Fixing Soak and Defence translations

--- modules/sheets/items/WFRP3eArmourSheet.js
+++ modules/sheets/items/WFRP3eArmourSheet.js
@@ -4,7 +4,7 @@ export default class WFRP3eArmourSheet extends ItemSheet
 	{
 		return mergeObject(super.defaultOptions,
 		{
-			template: "systems/wfrp3e/templates/armour-sheet.html",
+			template: "systems/wfrp3e/templates/armour-sheet.hbs",
 			//width: 530,
 			//height: 340,
 			classes: ["wfrp3e", "sheet", "item", "armour", "armour-item-sheet"]
