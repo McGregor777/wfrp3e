7319270b439e74a692b6edb74d504da961d875b3
Grégory Soupé <gregory.soupe@mailo.com>
2024-02-05 17:16:47 +0100

    Giving a greater initial height to Trapping sheets

--- modules/applications/items/WFRP3eTrappingSheet.js
+++ modules/applications/items/WFRP3eTrappingSheet.js
@@ -6,7 +6,7 @@ export default class WFRP3eTrappingSheet extends ItemSheet
 		{
 			template: "systems/wfrp3e/templates/applications/items/trapping-sheet.hbs",
 			//width: 530,
-			//height: 340,
+			height: 400,
 			classes: ["wfrp3e", "sheet", "item", "trapping"],
 			tabs: [
 				{group: "primary", navSelector: ".primary-tabs", contentSelector: ".sheet-body", initial: "main"},
