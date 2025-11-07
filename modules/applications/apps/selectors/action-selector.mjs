ff016176389644c12db1254cff5c3d15c9087933
McGregor777 <gregory.gauthier@mailo.com>
2025-11-07 13:40:23 +0100

    feat: Add actions from item collection to the action selector

--- modules/applications/apps/selectors/action-selector.mjs
+++ modules/applications/apps/selectors/action-selector.mjs
@@ -114,15 +114,33 @@ export default class ActionSelector extends Selector
 		const ownedActionNames = actor.itemTypes.action.map(action => action.name),
 			  actions = [];
 
+		for(const action of game.items.search({filters: [{field: "type", operator: "equals", value: "action"}]}))
+			if(await this.#filterAction(action, actor, ownedActionNames, options?.basic))
+				actions.push(action);
+
 		for(const pack of game.packs)
 			if(pack.documentName === "Item")
 				for(const action of await pack.getDocuments({type: "action"}))
-					if((options?.basic === true || !action.system.reckless.traits.includes(game.i18n.localize("TRAITS.basic")))
-						&& await action.checkRequirements({actor})
-						&& !ownedActionNames.includes(action.name))
+					if(await this.#filterAction(action, actor, ownedActionNames, options?.basic))
 						actions.push(action);
 
 		return actions;
+	}
+
+	/**
+	 * Verifies that an action fulfills the requirements to be shown in the selector.
+	 * @param {Item} action The action to check up.
+	 * @param {Actor} actor The actor concerned by the action selection.
+	 * @param {string[]} ownedActionNames The names of the actions owned action.
+	 * @param {boolean} allowBasic Whether basic actions are allowed in the selector.
+	 * @returns {Promise<boolean>}
+	 * @private
+	 */
+	static async #filterAction(action, actor, ownedActionNames, allowBasic)
+	{
+		return (allowBasic === true || !action.system.reckless.traits.includes(game.i18n.localize("TRAITS.basic")))
+			&& await action.checkRequirements({actor})
+			&& !ownedActionNames.includes(action.name);
 	}
 
 	/**
