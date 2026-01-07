c7d5254413a4513994ba6d02d75722f0b1679c50
Grégory Gauthier <gregory.gauthier@mailo.com>
2026-01-07 16:07:52 +0100

    doc: Clarify macros usage

--- modules/data/macros/embedded-item-creation-macro.mjs
+++ modules/data/macros/embedded-item-creation-macro.mjs
@@ -2,7 +2,7 @@ import ActiveEffectMacro from "./active-effect-macro.mjs";
 
 /**
  * The data model for an Embedded Item Creation Macro for an Active Effect.
- * @property {string} script The script that is executed whenever an embedded item is created for an Actor.
+ * @property {string} script The script that is executed whenever one or multiple embedded items are created for an Actor.
  * @property {string} type The type of Active Effect Macro, a value in ActiveEffectMacro.TYPES.
  */
 export default class EmbeddedItemCreationMacro extends ActiveEffectMacro
--- modules/data/macros/item-addition-macro.mjs
+++ modules/data/macros/item-addition-macro.mjs
@@ -2,7 +2,7 @@ import ActiveEffectMacro from "./active-effect-macro.mjs";
 
 /**
  * The data model for an Item Addition Macro for an Active Effect.
- * @property {string} script The script that is executed whenever an item is added to an Actor.
+ * @property {string} script The script that is executed whenever this item is added to an Actor.
  * @property {string} type The type of Active Effect Macro, a value in ActiveEffectMacro.TYPES.
  */
 export default class ItemAdditionMacro extends ActiveEffectMacro
