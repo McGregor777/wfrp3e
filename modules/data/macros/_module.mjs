c50aa3ed62fb6e9dd8db520698353be43ece3cfe
Grégory GAUTHIER <gregoryg@lepationumerique.com>
2026-04-23 14:46:03 +0200

    feat: New Reaction Macro

--- modules/data/macros/_module.mjs
+++ modules/data/macros/_module.mjs
@@ -16,6 +16,7 @@ export {default as ManualPostCheckRollMacro} from "./manual-post-check-roll-macr
 export {default as ManualPreCheckRollMacro} from "./manual-pre-check-roll-macro.mjs";
 export {default as ManualMacro} from "./manual-macro.mjs";
 export {default as PowerFavourAdjustmentMacro} from "./power-favour-adjustment-macro.mjs";
+export {default as ReactionMacro} from "./reaction-macro.mjs";
 export {default as RequirementMacro} from "./requirement-macro.mjs";
 export {default as StanceAdjustmentMacro} from "./stance-adjustment-macro.mjs";
 export {default as StartingSkillTrainingSelectionMacro} from "./starting-skill-training-selection-macro.mjs";
